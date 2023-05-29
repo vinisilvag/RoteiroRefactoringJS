4b952e2fb8e4fbe0a7f2ec9805841a4e811d6e60
Vinicius Gomes <vinicius.svgomes@gmail.com>
2023-05-29 17:45:31 -0300

    Commit 5 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -48,21 +48,34 @@ function gerarFaturaStr(fatura, pecas) {
     return creditos;   
   }
 
-  let totalFatura = 0;
-  let creditos = 0;
+  function calcularTotalFatura() {
+    let total = 0;
+
+    for (let apre of fatura.apresentacoes) {
+      total += calcularTotalApresentacao(apre);
+    }
+
+    return total;
+  }
+
+  function calcularTotalCreditos() {
+    let total = 0;
+
+    for (let apre of fatura.apresentacoes) {
+      total += calcularCredito(apre);
+    }
+
+    return total;
+  }
+
   let faturaStr = `Fatura ${fatura.cliente}\n`;
 
   for (let apre of fatura.apresentacoes) {
-    let total = calcularTotalApresentacao(apre);
-    creditos += calcularCredito(apre)
-
-    // mais uma linha da fatura
-    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-    totalFatura += total;
+    faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
   }
 
-  faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-  faturaStr += `Créditos acumulados: ${creditos} \n`;
+  faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+  faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
 
   return faturaStr;
 }
